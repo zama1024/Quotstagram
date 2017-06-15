84f23c87d40bb29d10439713de65445da411dafe
zama1024 <farshid_bd@hotmail.com>
2017-06-15 17:47:04 -0400

    User find by credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,3 +3,11 @@ class User < ActiveRecord::Base
   attr_reader :password
 
   validates :username, :password_digest, :session_token, presence: true
+  validates :username, :session_token, uniqueness: true
+
+  validates :password, length: {minimum: 6}, allow_nil: true
+
+  def self.find_by_credentials(credentials)
+    user = User.find_by(username: credentials[:username])
+    user && user.is_password?(credentials[:password]) ? user : nil
+  end
