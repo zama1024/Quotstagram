08b428dc5565de7022f2aebdab45cf4cac761364
zama1024 <farshid_bd@hotmail.com>
2017-06-15 18:09:13 -0400

    Ensure session token

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ActiveRecord::Base
 
   validates :username, :password_digest, :session_token, presence: true
   validates :username, :session_token, uniqueness: true
-
   validates :password, length: {minimum: 6}, allow_nil: true
+
+  after_initialize :ensure_session_token
 
   def self.find_by_credentials(credentials)
     user = User.find_by(username: credentials[:username])
