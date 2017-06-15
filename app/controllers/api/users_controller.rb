8e566d21095f960ccc49428c0b1208dcc9f327bc
zama1024 <farshid_bd@hotmail.com>
2017-06-15 18:52:25 -0400

    Add user controller

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,15 @@
+require 'byebug'
+class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      byebug
+    else
+      byebug
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username,:password)
+  end
+end
